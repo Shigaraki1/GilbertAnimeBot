0d7b6c4d536af4910ee140e59ff4ac4a19b536f1
ghoul-lover <88079604+ghoul-lover@users.noreply.github.com>
2021-11-21 09:56:53 +0530

    Update antiflood.py

--- GilbertAnimeBot/modules/antiflood.py
+++ GilbertAnimeBot/modules/antiflood.py
@@ -354,7 +354,7 @@ will result in restricting that user.
  `1w` = 1 week
  """
 
-__mod_name__ = "Anti-Flood"
+__mod_name__ = "A-Flood"
 
 FLOOD_BAN_HANDLER = MessageHandler(
     Filters.all & ~Filters.status_update & Filters.group, check_flood)
