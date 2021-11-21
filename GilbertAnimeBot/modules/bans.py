5b4eec8d040336ab297d7d3850b3d14c7e3d401e
ghoul-lover <88079604+ghoul-lover@users.noreply.github.com>
2021-11-21 10:00:46 +0530

    Update bans.py

--- GilbertAnimeBot/modules/bans.py
+++ GilbertAnimeBot/modules/bans.py
@@ -398,18 +398,7 @@ def selfunban(context: CallbackContext, update: Update) -> str:
     return log
 
 
-__help__ = """
- • `/punchme`*:* punches the user who issued the command
- • `/kickme`*:* kicks the user who issued the command
 
-*Admins only:*
- • `/ban <userhandle>`*:* bans a user. (via handle, or reply)
- • `/sban <userhandle>`*:* Silently ban a user. Deletes command, Replied message and doesn't reply. (via handle, or reply)
- • `/tban <userhandle> x(m/h/d)`*:* bans a user for `x` time. (via handle, or reply). `m` = `minutes`, `h` = `hours`, `d` = `days`.
- • `/unban <userhandle>`*:* unbans a user. (via handle, or reply)
- • `/kick <userhandle>`*:* Kicks a user out of the group, (via handle, or reply)
- • `/punch <userhandle>`*:* Punches a user out of the group, (via handle, or reply)
-"""
 
 BAN_HANDLER = CommandHandler(["ban", "sban"], ban)
 TEMPBAN_HANDLER = CommandHandler(["tban"], temp_ban)
