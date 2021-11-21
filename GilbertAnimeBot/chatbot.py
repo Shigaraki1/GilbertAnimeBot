b7eba9a49b6578a5a78ecfe5250cda27f78d28cd
ghoul-lover <88079604+ghoul-lover@users.noreply.github.com>
2021-11-21 18:24:56 +0530

    Create chatbot.py

---
+++ GilbertAnimeBot/chatbot.py
@@ -0,0 +1,10 @@
+__help__ = f"""
+Chatbot utilizes the CoffeeHouse API and allows Saitama to talk and provides a more interactive group chat experience.
+*Commands:* 
+*Admins only:*
+ • `/addchat`*:* Enables Chatbot mode in the chat.
+ • `/rmchat`*:* Disables Chatbot mode in the chat.
+Reports bugs at @{SUPPORT_CHAT}
+*Powered by CoffeeHouse* (https://coffeehouse.intellivoid.net/) from @Intellivoid
+"""
+__mod_name__ = "Chatbot
